5f546678735b47eafb2bfa38649bb939cd1e35b3
Moshe Kravchik <mkravchik@hotmail.com>
2024-05-21 17:04:14 +0300

    More features tested

--- src/main.c
+++ src/main.c
@@ -16,6 +16,17 @@ typedef struct _smoker {
   uint8_t cigarettesConsumed;
 } smoker;
 
+typedef struct _BF {
+  uint32_t x : 6;
+  uint32_t y : 10; // { y <= 900 };
+  uint32_t z : 16; // { y + z <= 60000 };
+} BF;
+
+typedef struct _boundedSum {//(UINT32 bound) {
+  uint32_t left;
+  uint32_t right; // { left <= bound && right <= bound - left };
+} boundedSum;
+
 int main(int argc, char *argv[]) {
     // Your code here
     printf("Hello, EverParse World!\n");
@@ -42,6 +53,22 @@ int main(int argc, char *argv[]) {
 
     smoker young_s = {2, 13};
     res = HelloWorldCheckSmoker((uint8_t*)&young_s, sizeof(young_s));
+    assert(res == FALSE);
+
+    BF bf = {1, 2, 3};
+    res = HelloWorldCheckBf((uint8_t*)&bf, sizeof(bf));
+    assert(res == TRUE);
+
+    BF bf2 = {1, 2, 60000};
+    res = HelloWorldCheckBf((uint8_t*)&bf2, sizeof(bf2));
+    assert(res == FALSE);
+
+    boundedSum bs = {1, 2};
+    res = HelloWorldCheckBoundedSum(4, (uint8_t*)&bs, sizeof(bs));
+    assert(res == TRUE);
+
+    boundedSum bs2 = {1, 4};
+    res = HelloWorldCheckBoundedSum(4, (uint8_t*)&bs2, sizeof(bs2));
     assert(res == FALSE);
 
     printf("All tests passed OK\n");
